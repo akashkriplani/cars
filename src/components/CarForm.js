61b1cbe6576a59764ea6c6b1a26fbe65d039c038
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-06 01:51:23 +0530

    Add a submit button to the car form

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { changeName, changeCost } from '../store';
+import { changeName, changeCost, addCar } from '../store';
 
 function CarForm() {
   const dispatch = useDispatch();
@@ -20,10 +20,15 @@ function CarForm() {
     dispatch(changeCost(carCost));
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(addCar({ name, cost }));
+  };
+
   return (
     <div className="car-form panel">
       <h4 className="subtitle is-3">Add Car</h4>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="field-group">
           <div className="field">
             <label className="label">Name</label>
@@ -33,6 +38,9 @@ function CarForm() {
             <label className="label">Cost</label>
             <input className="input is-expanded" type="number" value={cost || ''} onChange={handleCostChange} />
           </div>
+        </div>
+        <div className="field">
+          <button className="button is-link">Submit</button>
         </div>
       </form>
     </div>
